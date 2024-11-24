32fe03156c5e0bbecafc653c406d373e12cf605a
kazinayeem <mohammadalinayeem360@gmail.com>
2024-11-24 13:14:46 +0600

    change env

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -34,7 +34,7 @@ export const userRegisterController = async (req, res) => {
     const newUser = new User(payload);
     const result = await newUser.save();
 
-    const link = `http://localhost:5000/api/v1/user/activeuser/${result._id}`;
+    const link = `${process.env.URL}/api/v1/user/activeuser/${result._id}`;
     await sendMailConfig(email, "active link", link, link)
       .then(() => {
         return res.status(201).json({
