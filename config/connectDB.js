f951aa6b556f520eed003d402a299987851bcddd
kazinayeem <mohammadalinayeem360@gmail.com>
2024-11-24 13:51:41 +0600

    d

--- config/connectDB.js
+++ config/connectDB.js
@@ -4,19 +4,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-if (!process.env.MONGODB_URL) {
-  throw new Error("please provide MONGODB_URLin the .env file");
-}
+// if (!process.env.MONGODB_URL) {
+//   throw new Error("please provide MONGODB_URLin the .env file");
+// }
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    const url = "mongodb+srv://kazinayeem55085:zkdpuzYARJaZ5s0x@e-com.anq1k.mongodb.net/?retryWrites=true&w=majority&appName=e-com"
+
+    
+    await mongoose.connect(url, {
     
       writeConcern: { w: "majority" },
     });
     console.log("database connected");
   } catch (error) {
-    console.log("server error ", error);
+    console.log("server error :", error);
    
   }
 }
